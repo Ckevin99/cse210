90273a1af220cc88ef15c71e8636081ea6ef7b0a
Skill99 <kevincorreia99@outlook.com>
2023-02-28 21:57:04 -0300

    preparation 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,21 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Please, type Your Grade!");
+        int grade = int.Parse(Console.ReadLine());
+
+        if (grade>=90){
+            Console.WriteLine("A grade");
+        }else if(grade>=80 && grade<90){
+            Console.WriteLine("B grade");
+        }else if(grade>=70 && grade<80){
+            Console.WriteLine("C grade");
+        }else if(grade>=60 && grade<70){
+            Console.WriteLine("D grade");
+            
+        }else{
+            Console.WriteLine("F grade");
+        }
+
     }
 }
