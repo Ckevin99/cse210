edabdf651e497bc5959c24aba7a527ee92973c79
Skill99 <kevincorreia99@outlook.com>
2023-03-08 20:31:46 -0300

    week3 commit

---
+++ prove/Develop03/Fraction.cs
@@ -0,0 +1,42 @@
+using System;
+
+public class Fraction{
+        private int _top;
+        private int _bottom;
+        
+    public Fraction(){
+        _top = 1;
+        _bottom = 1;
+        }
+    
+    public Fraction(int top){
+        _top = top;
+        _bottom = 1;
+
+     }
+    public Fraction(int top, int bottom){
+        _top = top;
+        _bottom = bottom;
+        }
+    public void GetTop(){
+        Console.WriteLine(_top);
+    }
+    public void GetBottom(){
+        Console.WriteLine(_bottom);
+    }
+    public void SetTop(int top){
+        _top = top;
+    }
+    public void SetBottom(int bottom){
+        _bottom = bottom;
+    }
+    
+    public string GetFractionalString(){
+        return $"{_top}/{_bottom}";
+    }
+    public double GetDecimalValue(){
+        
+        return (double)_top / (double)_bottom;
+    }
+
+     }
