b45dbe8b4e64af2fc7978ef2cc7b907979c2f154
Skill99 <kevincorreia99@outlook.com>
2023-04-03 06:31:06 -0300

    fundamentals 2 classes

---
+++ final/Foundation2/Product.cs
@@ -0,0 +1,38 @@
+using System;
+
+
+
+class Product
+{
+    private string _name;
+    private string _productID;
+    private int _price;
+    private int _quantity;
+
+
+
+
+    public int TotalPrice()
+    {
+        return _price*_quantity;
+
+
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
